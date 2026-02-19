139b716da76652df5f2c2850e02f82cc6722554c
Mikołaj Michalak <majkanart117@gmail.com>
2026-02-19 22:33:49 +0100

    Deploy to Github pages

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  const repoName = 'Mini-Budget-Tracker'
+
+  return {
+    base: process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/',
+    plugins: [react()],
+  }
 })
